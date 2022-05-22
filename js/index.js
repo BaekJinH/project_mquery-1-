8fc52c3bfd7e985d013f15cf1f7bb152d924e266
BaekJinH <88886246+BaekJinH@users.noreply.github.com>
2022-05-22 23:52:41 +0900

    slider_menu start

--- js/index.js
+++ js/index.js
@@ -1,5 +1,45 @@
 $(document).ready(function(){
-    $('.main4_img').hover(function(){
-        $('.icon').fadeIn()
+    $('.main4_img1').hover(function(){
+        $('.icon1').fadeIn();
+        $('.icon2').hide();
+        $('.icon3').hide();
+        $('.icon4').hide();
+    })
+    $('.main4_img2').hover(function(){
+        $('.icon2').fadeIn();
+        $('.icon1').hide();
+        $('.icon3').hide();
+        $('.icon4').hide();
+    })
+    $('.main4_img3').hover(function(){
+        $('.icon3').fadeIn();
+        $('.icon2').hide();
+        $('.icon1').hide();
+        $('.icon4').hide();
+    })
+    $('.main4_img4').hover(function(){
+        $('.icon4').fadeIn();
+        $('.icon2').hide();
+        $('.icon3').hide();
+        $('.icon1').hide();
     })
 })
+
+
+
+
+
+
+$(function(){
+    $('.menu').click(function(){
+        $('.slide_menu').slideToggle()
+    })
+})
+
+function open_menu(){
+    document.getElementsByClassName('slide_menu')[0].style.width='100%';
+}
+
+function close_menu(){
+    document.getElementsByClassName('slide_menu')[0].style.width='0%';
+}
